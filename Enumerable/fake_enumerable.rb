022116d5823313c469767a6c323d6e6605aa881a
Renée Hendricksen <renee@nird.us>
2014-09-28 17:47:05 -0700

    renee's enumerable solution

--- Enumerable/fake_enumerable.rb
+++ Enumerable/fake_enumerable.rb
@@ -1,21 +1,52 @@
 module FakeEnumerable
-  def each
-    if block_given?
-      @data.each { |e| yield(e) }
-    else
-      FakeEnumerator.new(self, :each)
-    end
-  end
 
   def map
+    return each unless block_given?
+    matched = []
+    each do |i|
+      matched << yield(i)
+    end
+    matched
   end
 
   def select
+    return each unless block_given?
+    matched = []
+    each do |i|
+      matched << i if yield(i)
+    end
+    matched
   end
 
   def sort_by
+    return each unless block_given?
+    sorted = {}
+    each do |i|
+      sorted[i] = yield(i)
+    end
+    sorted.sort_by{|k,v| v}.map{|a| a.first}
   end
 
-  def reduce(*args)
+  def reduce *args, &block
+    enum = each
+    return looper args.first, enum, args.last if args.length == 2
+    return looper args.first, enum, block if block_given?
+    return looper nil, enum, args.last unless args.empty?
+    enum
+  end
+  alias_method :inject, :reduce
+
+private
+  def looper value, enum, method
+    loop do
+      value ||= enum.next
+      item = enum.next
+      if method.respond_to? :call
+        value = method.call value, item
+      else
+        value = value.send(method, item)
+      end
+    end
+    value
   end
 end
