c74986937ba0929cf1b513ab70268d09b1a330f7
Kerri Miller <kerrizor@kerrizor.com>
2014-09-28 23:21:02 -0700

    add spec for delegation example (and correct a few bugs along the way..)

--- Ruby_Call_Chain_and_Method_Missing/delegation.rb
+++ Ruby_Call_Chain_and_Method_Missing/delegation.rb
@@ -1,22 +1,22 @@
 class Invoice
   attr_reader :amount_owed, :amount_paid
 
-  def initialize options=Hash.new(0.0)
+  def initialize options = Hash.new(0.0)
     @amount_owed = options[:amount_owed]
     @amount_paid = options[:amount_paid]
   end
 
-  def total_amout
-   taxed_amount_owed - @amount_paid
+  def total_amount
+   (@amount_owed + tax_amount_owed) - @amount_paid
   end
 
-  def taxed_amount_owed
-    @amount_owed * tax_rate
+  def tax_amount_owed
+    (@amount_owed * tax_rate).round(2)
   end
 
   def tax_rate
     # some crazy lookup based on state and country
-    state_tax_rate("WA") * country_tax_rate "USA"
+    (state_tax_rate("WA") + country_tax_rate("USA")).round(2)
   end
 
   def state_tax_rate state
