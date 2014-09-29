ea2229c33f4ffb03db6694bca356e95ad70051ce
Kerri Miller <kerrizor@kerrizor.com>
2014-09-29 10:53:48 -0700

    solving with delegation

--- Ruby_Call_Chain_and_Method_Missing/delegation.rb
+++ Ruby_Call_Chain_and_Method_Missing/delegation.rb
@@ -1,4 +1,10 @@
+require 'forwardable'
+
 class Invoice
+  extend Forwardable
+
+  def_delegators "TaxCalculator", :tax_rate, :state_tax_rate, :country_tax_rate
+
   attr_reader :amount_owed, :amount_paid
 
   def initialize options = Hash.new(0.0)
@@ -13,17 +19,20 @@ class Invoice
   def tax_amount_owed
     (@amount_owed * tax_rate).round(2)
   end
+end
 
-  def tax_rate
+
+class TaxCalculator
+  def self.tax_rate
     # some crazy lookup based on state and country
     (state_tax_rate("WA") + country_tax_rate("USA")).round(2)
   end
 
-  def state_tax_rate state
+  def self.state_tax_rate state
     0.05
   end
 
-  def country_tax_rate country
+  def self.country_tax_rate country
     0.10
   end
 end
