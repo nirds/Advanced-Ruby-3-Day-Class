34886b2e1c869d0c0c64ef6a8d6f7e4615151984
Renée Hendricksen <github@nird.us>
2014-09-29 16:55:42 -0700

    more potential breaking changes to spec

--- Enumerable/spec/fake_enumerable_spec.rb
+++ Enumerable/spec/fake_enumerable_spec.rb
@@ -11,15 +11,24 @@ describe "FakeEnumerable" do
 
   it "supports map" do
     @list.map { |x| x + 1 }.must_equal([4,5,8,14,43])
+    
+    enum = @list.map
+    enum.next.must_equal(3)
   end
 
   it "supports sort_by" do
     # ASCII sort order
     @list.sort_by { |x| x.to_s }.must_equal([13, 3, 4, 42, 7])
+    
+    enum = @list.sort_by
+    enum.next.must_equal(3)
   end
 
   it "supports select" do
     @list.select { |x| x.even? }.must_equal([4,42])
+    
+    enum = @list.select
+    enum.next.must_equal(3)
   end
 
   it "supports reduce" do
@@ -28,10 +37,6 @@ describe "FakeEnumerable" do
     @list.reduce { |s,e| s + e }.must_equal(69)
     @list.reduce(-10) { |s,e| s + e }.must_equal(59)
     @list.reduce(-10, :+).must_equal(59)
-    
-    enum = @list.reduce
-    enum.next.must_equal(3)
-    
     @list.reduce(:numbers){|s,e| s.to_s + e.to_s }.must_equal("numbers3471342")
     @list.reduce("numbers"){|s,e| s + e.to_s }.must_equal("numbers3471342")
   end
