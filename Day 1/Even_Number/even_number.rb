47bdd270c098713ee73875c57fb193c2203b2301
Mark Washburn <mark.washburn@opower.com>
2014-10-20 13:22:34 -0400

    moved check for invalid even numbers to "new" method to avoid allocating memory for invalid objects

--- Day 1/Even_Number/even_number.rb
+++ Day 1/Even_Number/even_number.rb
@@ -2,11 +2,12 @@ class EvenNumber
   include Comparable
   attr_reader :value
 
-  def initialize(input)
-    unless input.is_a?(Integer) && input.even?
-      raise TypeError
-    end
+  def self.new(input)
+    raise TypeError unless input.is_a?(Integer) && input.even?
+    super
+  end
 
+  def initialize(input)
     @value = input
   end
 
