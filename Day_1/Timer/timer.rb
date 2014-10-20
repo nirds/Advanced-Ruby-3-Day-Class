c74c51aada5372cced6aae5728c4ea9a8a0938f1
Mark Washburn <mark.washburn@opower.com>
2014-10-20 14:52:44 -0400

    stubbed out slow tests

--- Day_1/Timer/timer.rb
+++ Day_1/Timer/timer.rb
@@ -1,9 +1,12 @@
 module Timer
   def self.time_code(times = 1)
-    start = Time.now
+    total_run_time = 0
+
     times.times do
+      start = Time.now
       yield
+      total_run_time += (Time.now - start)
     end
-    (Time.now - start)/times
+    total_run_time/times
   end
 end
--- Day_1/Timer/timer_spec.rb
+++ Day_1/Timer/timer_spec.rb
@@ -8,8 +8,9 @@ describe Timer do
   end
 
   it 'runs sleep(1) for 1 second' do
+    allow(Time).to receive(:now).and_return(0,1)
     start = Time.now
-    Timer.time_code { sleep(1) }
+    Timer.time_code { }
     expect(Time.now - start).to be_within(0.01).of(1)
   end
 
@@ -21,6 +22,7 @@ describe Timer do
   end
 
   it 'averages runtime correctly' do
-    expect(Timer.time_code(5) { sleep(1) }).to be_within(0.1).of(1)
+    allow(Time).to receive(:now).and_return(*(0..5))
+    expect(Timer.time_code(3) { }).to be_within(0.1).of(1)
   end
 end
